d8a65f46cae2b50d17d62fbf3632313d0da3059c
Ben Rams <33656459+BenRams@users.noreply.github.com>
2018-03-31 18:09:30 +0200

    saisie de texte

--- hub/main.cpp
+++ hub/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-
+#include<string>
 using namespace std;
 
 int main()
 {
+    std::string texte;
     for(int i=0;i<6;i++)
     {
         cout << "Yo" << endl;
     }
+    std::cin>>texte;
+    std::cout<<texte;
     return 0;
 }
